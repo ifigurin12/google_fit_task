a2ab573fe5e6eedccf1b6d3b6cda8241115ff542
ifigurin12 <ifigurin1411@gmail.com>
2023-09-17 01:45:28 +0300

    fix: Исправлена ошибка с java main activity

--- android/app/src/main/kotlin/com/example/google_fit_test_task/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/google_fit_test_task/MainActivity.kt
@@ -1,18 +1,13 @@
-import androidx.annotation.NonNull
+package com.example.google_fit_test_task
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
 
 class MainActivity: FlutterActivity() {
-    static const platform = MethodChannel('samples.flutter.dev/battery');
-
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-                call, result ->
-            // Note: this method is invoked on the main thread.
-            // TODO
-        }
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
 
 }
