69764a459a373221bdc5962f3a27a9c7750ced84
ifigurin12 <ifigurin1411@gmail.com>
2023-09-21 16:35:12 +0300

    add: Добавил аутентификацию

--- android/app/src/main/kotlin/com/example/google_fit_test_task/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/google_fit_test_task/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.google_fit_test_task
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.autofill.AutofillManager
 import androidx.annotation.NonNull
-import androidx.annotation.RequiresApi
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -23,111 +19,47 @@ import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL_AUTH = "auth_google"
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL_AUTH).setMethodCallHandler {
-                call, result ->
-            if (call.method == "signIn") {
-                val intent : Intent = Intent(this, AuthGoogleApi::class.java)
-                startActivityForResult(intent, 1)
-                var acc = AuthGoogleApi.account
-                if(acc == null)
-                {
-                    result.success("Nothing")
-                }
-                else
-                {
-                    result.success("Register: email - ${acc.email}")
-                }
 
-            }
+    val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 11
 
-        }
-
-    }
     val fitnessOptions = FitnessOptions.builder()
         .addDataType(DataType.TYPE_HEART_RATE_BPM, FitnessOptions.ACCESS_READ)
         .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
         .addDataType(DataType.TYPE_WEIGHT, FitnessOptions.ACCESS_READ)
         .build()
+    var account : GoogleSignInAccount? = null;
 
-}
-
-
-class AuthGoogleApi : FlutterActivity() {
-
-    private val serverClientId  = "460023958134-hovmn9ka16d6t0oveur4b6o48hrpgki5.apps.googleusercontent.com"
-    companion object {
-
-        var account : GoogleSignInAccount? = null
-    }
-
-    lateinit var mGoogleSignInClient : GoogleSignInClient
-    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestScopes(
-            Scope("https://www.googleapis.com/auth/fitness.activity.read"),
-            Scope("https://www.googleapis.com/auth/fitness.heart_rate.read"),
-            Scope("https://www.googleapis.com/auth/fitness.body.read"),
-        )
-        .requestServerAuthCode(serverClientId)
-        .build()
-
-
-    override fun onStart() {
-        super.onStart()
-        account = GoogleSignIn.getLastSignedInAccount(this)
-        if(account == null)
-        {
-            signIn()
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL_AUTH
+        ).setMethodCallHandler { call, result ->
+            if (call.method == "signIn") {
+                account = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+                if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) {
+                    GoogleSignIn.requestPermissions(
+                        this,
+                        GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                        account,
+                        fitnessOptions
+                    )
+                }
+                if (accountCheck())
+                {
+                    result.success(true)
+                }
+                else
+                {
+                    result.success(false)
+                }
+            }
         }
     }
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val afm = context.getSystemService(AutofillManager::class.java)
-        afm?.requestAutofill(View(context))
-        if (!afm.isAutofillSupported) { // true
-
-            /*if (!afm.hasEnabledAutofillServices()) // false
-            {
-                finish()
-            }*/ // Вот эти 2 проверки выдает false
-
-            /*if (!afm.showAutofillDialog(View(context))) // false
-             {
-                 finish()
-              }*/
-        }
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
+    fun accountCheck(): Boolean {
+        return account != null && GoogleSignIn.hasPermissions(account, fitnessOptions)
     }
-    private fun signIn() {
-        val signInIntent: Intent = mGoogleSignInClient.signInIntent
-        startActivityForResult(signInIntent, 1)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 1) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
-            finish()
-        }
-    }
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-
-        } catch (e: ApiException) {
-        }
-
-    }
-
 
 }
 
