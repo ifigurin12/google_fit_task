c457806f944372923946c3329365b7367755b669
ifigurin12 <ifigurin1411@gmail.com>
2023-09-24 01:29:33 +0300

    add: попытка получить данные все также провальна, но продолжаем

--- android/app/src/main/kotlin/com/example/google_fit_test_task/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/google_fit_test_task/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.google_fit_test_task
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.util.Log
 import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
 import com.google.android.gms.fitness.data.DataSet
 import com.google.android.gms.fitness.data.DataType
 import com.google.android.gms.fitness.request.DataReadRequest
-import com.google.android.gms.tasks.Tasks.await
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -81,43 +83,62 @@ class MainActivity: FlutterActivity() {
         Fitness.getConfigClient(this, getGoogleAccount())
             .disableFit()
             .addOnSuccessListener {
+                Log.i("SIGN_OUT", "Удалось выйти")
                 isSignOut = true
             }
             .addOnFailureListener { _ ->
                 isSignOut = true
+                Log.i("SIGN_OUT", "Удалось выйти")
             }
         return isSignOut
     }
 
     private fun getCountData() {
+        checkFitPermission()
         val end = LocalDateTime.now()
         val start = end.minusYears(1)
         val endSeconds = end.atZone(ZoneId.systemDefault()).toEpochSecond()
         val startSeconds = start.atZone(ZoneId.systemDefault()).toEpochSecond()
 
         val readRequest = DataReadRequest.Builder()
-            .aggregate(DataType.TYPE_WEIGHT)
-            .setTimeRange(startSeconds, endSeconds, TimeUnit.SECONDS)
+            .aggregate(DataType.TYPE_HEART_RATE_BPM)
+            .setTimeRange(startSeconds, endSeconds, TimeUnit.DAYS)
             .bucketByTime(1, TimeUnit.DAYS)
             .build()
         val account = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
         val response = Fitness.getHistoryClient(this, account)
             .readData(readRequest)
             .addOnSuccessListener { response ->
-                // The aggregate query puts datasets into buckets, so flatten into a
-                // single list of datasets
+               dumpDataSet(response.getDataSet(DataType.TYPE_HEART_RATE_BPM))
 
             }
     }
+    private fun checkFitPermission()
+    {
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
+                    == PackageManager.PERMISSION_DENIED)) {
+            ActivityCompat.requestPermissions(activity,
+                arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                1)
+        }
+        else if ((ContextCompat.checkSelfPermission(activity, Manifest.permission.BODY_SENSORS)
+                    == PackageManager.PERMISSION_DENIED)) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.BODY_SENSORS),
+                1)
+        }
+    }
     fun dumpDataSet(dataSet: DataSet) {
         val TAG = "FITNESS API DATA"
-        // Log.i(TAG, "Data returned for Data type: ${dataSet.dataType.}")
         for (dp in dataSet.dataPoints) {
             Log.i(TAG, "Data point:")
             for (field in dp.dataType.fields) {
                 Log.i(TAG, "\tField: ${field.name.toString()} Value: ${dp.getValue(field)}")
             }
         }
+        Log.i(TAG, "Data returned for Data type: ${dataSet.dataType}")
+        Log.i(TAG, dataSet.toString())
+
     }
 }
 
